8944458467854c099022696a85c0546551982ee2
kravtsov <a.kravtsov@spichka.biz>
2019-10-08 22:51:50 +0500

    action on the back button

--- app/src/main/java/space/kravtsov/quiz/GameLevels.java
+++ app/src/main/java/space/kravtsov/quiz/GameLevels.java
@@ -1,8 +1,11 @@
 package space.kravtsov.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +17,31 @@ public class GameLevels extends AppCompatActivity {
         setContentView(R.layout.gamelevels);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Команда для кнопки
+
+                try{
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);
+                } catch(Exception e) {
+
+                }
+            }
+        });
+    }
+
+    //Системная кнопка
+    @Override
+    public void onBackPressed() {
+        try{
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);
+        } catch(Exception e) {
+
+        }
     }
 }
