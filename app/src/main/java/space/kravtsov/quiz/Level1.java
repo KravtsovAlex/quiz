b85e2959e52ffb39540c68895cb8932465c5e332
kravtsov <a.kravtsov@spichka.biz>
2019-10-11 00:33:30 +0500

    level chain

--- app/src/main/java/space/kravtsov/quiz/Level1.java
+++ app/src/main/java/space/kravtsov/quiz/Level1.java
@@ -2,9 +2,12 @@ package space.kravtsov.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Level1 extends AppCompatActivity {
 
@@ -15,5 +18,20 @@ public class Level1 extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Команда для кнопки
+
+                try{
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent);
+                } catch(Exception e) {
+
+                }
+            }
+        });
     }
 }
